d02efc2ffa66707cfe6b253846fb4b756b6bee15
jakir540 <jakirhossainmd540@gamil.com>
2023-11-02 18:21:27 +0600

    draggeble functionality added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="flex ">
-    <div className="px-3">
-      <label>
-        <input type="checkbox" />
-      </label>
-    </div>
-    <h3>File Selected</h3>
-  </div>
+    <>
+      <div className="flex h-20 items-center ">
+        <div>
+          <h2 className="text-2xl font-bold">Gallery</h2>
+        </div>
+
+        <div className="flex items-center">
+          <div className="px-3 flex items-center">
+            <label>
+              <input className="w-5 h-5" type="checkbox" />
+            </label>
+          </div>
+          <div>
+            <h3 className="text-2xl font-bold">File Selected</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-right ">
+        <h3 className="text-2xl font-bold text-red-600 mt-[-50px] pb-3">Delete File</h3>
+      </div>
+      <hr className="py-4" />
+    </>
   );
 };
 
--- src/components/Home/Gallery/Gallery.jsx
+++ src/components/Home/Gallery/Gallery.jsx
@@ -3,14 +3,43 @@ import images from "/public/image.json";
 import AddImage from "../../AddImage/AddImage";
 
 const Gallery = () => {
-  const [image, setImage] = useState(images);
+  const [gallery, setGallery] = useState(images);
+
+  const handleDragStart = (e, index) => {
+    e.dataTransfer.setData("index", index);
+    // console.log(e, index);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    // console.log(e);
+  };
+
+  const handleDrop = (e, targetIndex) => {
+    e.preventDefault();
+    const draggedIndex = e.dataTransfer.getData("index");
+    const updatedGallery = [...gallery];
+
+    // Reorder the gallery all image array
+    const [draggedImage] = updatedGallery.splice(draggedIndex, 1);
+    
+    updatedGallery.splice(targetIndex, 0, draggedImage);
+
+    setGallery(updatedGallery);
+  };
 
   return (
     <div className="bg-slate-100">
-     
       <div className=" relative flex  gap-6 flex-wrap justify-center">
-        {image.map((img) => (
-          <div className="border border-spacing-3 shadow-xl relative rounded-md">
+        {gallery.map((img, index) => (
+          <div
+            onDragStart={(e) => handleDragStart(e, index)}
+            onDragOver={handleDragOver}
+            onDrop={(e) => handleDrop(e, index)}
+            draggable={true}
+            key={img.id}
+            className="border border-spacing-3 shadow-xl relative rounded-md"
+          >
             <figure className="p-3 mx-auto">
               <img
                 src={img.image}
@@ -22,7 +51,7 @@ const Gallery = () => {
 
             <div className="absolute top-5 left-5">
               <label>
-                <input type="checkbox" />
+                <input className="w-5 h-5" type="checkbox" />
               </label>
             </div>
           </div>
